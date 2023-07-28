a2edd533ead311c9307a6d111b1fbf2e80b57114
Thamires Febbo <thamiresfebbo@hotmail.com>
2023-07-27 22:43:03 -0300

    atualizaçao com validador

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import validator from './validator.js';
 
-console.log(validator);
-console.log ("Oláaa")
+// Função para validar o formulários
+function validar(event) {
+  event.preventDefault();
+  const ccNumberInput = document.getElementById('creditCardNumber');
+  const validationResult = document.getElementById('validationResult');
+
+  const ccNumber = ccNumberInput.value.trim();
+  const isValid = validator.isValid(ccNumber);
+  //const maskedNumber = validator.maskify(creditCardNumber);
+  //ccNumberInput.value = maskedNumber;
+
+  if (!ccNumber) {
+    validationResult.textContent = 'Por favor, preencha o número do cartão.';
+  } else if (isValid) {
+    validationResult.textContent = 'Parabéns, cartão validado!';
+  } else {
+    validationResult.textContent = 'Cartão inválido.';
+  }
+};
+
+// Obter o form do formulário
+const form = document.getElementById('creditCardForm');
+
+// ao clicar enviar chama as funções de cima
+form.addEventListener('submit', validar);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,33 @@
 const validator = {
-  // ...
+  isValid: function (creditCardNumber) {
+    let sum = 0;
+    let shouldDouble = false;
+    for (let i = creditCardNumber.length - 1; i >= 0; i--) {
+      let digit = parseInt(creditCardNumber.charAt(i));
+
+      if (shouldDouble) {
+        digit *= 2;
+        if (digit > 9) digit -= 9;
+      }
+
+      sum += digit;
+      shouldDouble = !shouldDouble;
+    }
+
+    return sum % 10 === 0;
+  },
+
+  //maskify: function (creditCardNumber) {
+   // if (creditCardNumber.length <= 4) {
+   //   return creditCardNumber;
+   // }
+
+   // const lastFourDigits = creditCardNumber.slice(-4);
+   // const maskedDigits = creditCardNumber.slice(0, -4).replace(/\d/g, '#');
+   // const maskedNumber = maskedDigits + lastFourDigits;
+
+   // return maskedNumber;
+  //},
 };
 
 export default validator;
