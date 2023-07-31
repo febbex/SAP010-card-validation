3df7301c2044a4d8dfd8044aab613f8a0224913e
Thamires Febbo <thamiresfebbo@hotmail.com>
2023-07-31 02:45:27 -0300

    atualizaçoes na interface

--- src/index.js
+++ src/index.js
@@ -2,26 +2,51 @@ import validator from './validator.js';
 
 // Função para validar o formulários
 function validar(event) {
-  event.preventDefault(); //evita enviar o formuário antes de validar 
+  event.preventDefault(); //'event.preventDefault' evita enviar o formuário antes de validar 
   const ccNumberInput = document.getElementById('creditCardNumber');
   const validationResult = document.getElementById('validationResult');
 
   const ccNumber = ccNumberInput.value.trim(); //trim() barrar se tiver espaços em branco
   const isValid = validator.isValid(ccNumber);
-  //const maskedNumber = validator.maskify(creditCardNumber);
-  //ccNumberInput.value = maskedNumber;
 
-  if (!ccNumber) {
+  if (!ccNumber) { // if verifica se é verdadeiro '!'verifica se está vazio o campo número
     validationResult.textContent = 'Por favor, preencha o número do cartão.';
   } else if (isValid) {
-    validationResult.textContent = 'Parabéns, cartão validado!';
+    validationResult.textContent = 'Parabéns, cartão validado!'; // se a condição anterior for falso o bloco de código dentro do else if será executado
   } else {
-    validationResult.textContent = 'Cartão inválido.';
+    validationResult.textContent = 'Cartão inválido.'; // se isValid for falso o bloco else final será executado
   }
-};
+}
 
 // Ref do formulário
 const form = document.getElementById('creditCardForm');
 
+
 // ao clicar enviar chama as funções de cima
 form.addEventListener('submit', validar);
+
+//Espelho do cartão
+document.querySelector('.form-control').oninput = () =>{
+  document.querySelector('.card-number-box').innerText = document.querySelector('.form-control').value;
+}
+  document.querySelector('.Nome-input').oninput = () =>{
+    document.querySelector('.card-holder-name').innerText = document.querySelector('.Nome-input').value;
+
+}
+document.querySelector('.meses-input').oninput = () =>{
+  document.querySelector('.exp-month').innerText = document.querySelector('.meses-input').value;
+}
+document.querySelector('.Ano').oninput = () =>{
+  document.querySelector('.exp-year').innerText = document.querySelector('.Ano').value;
+}
+document.querySelector('.cvc-input').onmouseenter = () =>{
+  document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(180deg)';
+  document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(0deg)';
+}
+document.querySelector('.cvc-input').onmouseleave = () =>{
+  document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(0deg)';
+  document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(180deg)';
+}
+document.querySelector('.cvc-input').oninput = () =>{
+  document.querySelector('.cvc-box').innerText = document.querySelector('.cvc-input').value;
+};
--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,8 @@
 const validator = {
   isValid: function (creditCardNumber) {
-    let somar = 0;
+    let soma = 0;
     let dobrar = false;
+    // 'for' = a loop
     for (let i = creditCardNumber.length - 1; i >= 0; i--) {
       let digit = parseInt(creditCardNumber.charAt(i));
 
@@ -10,24 +11,14 @@ const validator = {
         if (digit > 9) digit -= 9;
       }
 
-      somar += digit;
+      soma += digit;
       dobrar = !dobrar;
     }
 
-    return somar % 10 === 0;
+    return soma % 10 === 0; // Após o loop se a soma for divisível por 10 sem deixar resto o número do cartão é válido.
+    // Se a soma não for divisível por 10 sem deixar resto, o retorno será false automaticamente
   },
 
-  //maskify: function (creditCardNumber) {
-   // if (creditCardNumber.length <= 4) {
-   //   return creditCardNumber;
-   // }
-
-   // const lastFourDigits = creditCardNumber.slice(-4);
-   // const maskedDigits = creditCardNumber.slice(0, -4).replace(/\d/g, '#');
-   // const maskedNumber = maskedDigits + lastFourDigits;
-
-   // return maskedNumber;
-  //},
 };
 
 export default validator;
